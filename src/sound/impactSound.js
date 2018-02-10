75be2a9412e9670becb820628e678bf3ae35999c
gitkonst <34488160+gitkonst@users.noreply.github.com>
2018-02-10 22:50:22 +0300

    Mad impact sound quiter on mobile

--- src/sound/impactSound.js
+++ src/sound/impactSound.js
@@ -1,8 +1,13 @@
-const IMPACT_SOUND_FILE = "sounds/lasrhit2_mod.wav";
-const IMPACT_SOUND_VOLUME = 0.04; // Range is 0...1
+import isMobile from "../isMobile";
 
-const impactAudio = new Audio(IMPACT_SOUND_FILE);
-impactAudio.volume = IMPACT_SOUND_VOLUME;
+const SOUND_FILE = "sounds/lasrhit2_mod.wav";
+const VOLUME_PC = 0.04; // Range is 0...1
+const VOLUME_MOBILE = 0.02; // Range is 0...1
+
+const VOLUME = isMobile() ? VOLUME_MOBILE : VOLUME_PC;
+
+const impactAudio = new Audio(SOUND_FILE);
+impactAudio.volume = VOLUME;
 
 function playImpactSound() {
   impactAudio.currentTime = 0;
